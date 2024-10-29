d13a9cc03944b382082c78e2fe708e7a4f0c99dc
Starmoon0911 <starmoon9159@gmail.com>
2024-10-29 22:02:10 +0800

    Add ascii-table

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 // src/app.ts
-import express, { Application } from 'express';
+import express, { Application, Router } from 'express';
 import path from 'path';
 import fs from 'fs/promises';
+import Table from 'ascii-table';
 
 const app: Application = express();
 
-async function registerRoutes(dir: string, app: Application, baseRoute = '') {
+async function registerRoutes(dir: string, app: Application, baseRoute = '', table: Table) {
   const files = await fs.readdir(dir, { withFileTypes: true });
 
   for (const file of files) {
@@ -13,17 +14,27 @@ async function registerRoutes(dir: string, app: Application, baseRoute = '') {
     const routePath = baseRoute + '/' + file.name.replace(/\.[tj]s$/, '');
 
     if (file.isDirectory()) {
-      await registerRoutes(fullPath, app, routePath); 
+      await registerRoutes(fullPath, app, routePath, table); 
     } else {
       const routeHandler = await import(fullPath);
       app.use(routePath, routeHandler.default);
+      
+
+      routeHandler.default.stack.forEach((middleware: any) => {
+        if (middleware.route) {
+          const methods = Object.keys(middleware.route.methods).map(method => method.toUpperCase()).join(', ');
+          const status = 'Registered';
+          table.addRow(routePath, methods, status);
+        }
+      });
     }
   }
 }
-
 (async () => {
-  await registerRoutes(path.join(__dirname, 'routes'), app);
-
+  const table = new Table();
+  table.setHeading('API Route', 'Method', 'Status');
+  await registerRoutes(path.join(__dirname, 'apis'), app, '', table);
+  console.log(table.toString());
 })();
 
 export default app;
