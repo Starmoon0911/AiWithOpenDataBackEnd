ae009e729fc4e3f053ff43ecc7042d7e5cb44c14
Starmoon9159 <112765789+Starmoon9159@users.noreply.github.com>
2024-11-02 22:21:40 +0800

    fix news update error

--- src/route/api/v1/news/executive.ts
+++ src/route/api/v1/news/executive.ts
@@ -1,19 +1,29 @@
 import EYnewsModel from '../../../../database/schemas/EYnews';
-import express from 'express'
-import fetchEYNews from '../../../../fetch/getEYnews';
+import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    const limit = parseInt(req.query.limit as string) || 5;
+    const page = parseInt(req.query.page as string) || 1;
     try {
+        const skip = (page - 1) * limit;
+        
         const news = await EYnewsModel
             .find()
-            .sort({ date: -1 })
-            .limit(5);
-        res.status(200).json({ success: true, data: news });
+            .sort({ date: -1 }) 
+            .skip(skip)          
+            .limit(limit);        
+        const total = await EYnewsModel.countDocuments();
+        res.status(200).json({
+            success: true,
+            data: news,
+            total: total,
+            page: page,
+            totalPages: Math.ceil(total / limit)
+        });
     } catch (error) {
-        res.status(500).json({ success: false, data: error })
+        res.status(500).json({ success: false, data: error.message });
     }
-})
+});
 
-export default router
-
+export default router;
