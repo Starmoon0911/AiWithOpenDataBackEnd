cb50b34fdd6c22fcc86c535c3697518b59b2ec48
Starmoon9159 <112765789+Starmoon9159@users.noreply.github.com>
2024-11-02 23:24:25 +0800

    fix news update error

--- src/route/api/v1/news/executive.ts
+++ src/route/api/v1/news/executive.ts
@@ -6,13 +6,15 @@ router.get('/', async (req, res) => {
     const limit = parseInt(req.query.limit as string) || 5;
     const page = parseInt(req.query.page as string) || 1;
     try {
+        if (limit > 30) res.status(400).json({ success: false, data: "回傳資料數超過限制!(30)" })
+
         const skip = (page - 1) * limit;
-        
+
         const news = await EYnewsModel
             .find()
-            .sort({ date: -1 }) 
-            .skip(skip)          
-            .limit(limit);        
+            .sort({ date: -1 })
+            .skip(skip)
+            .limit(limit);
         const total = await EYnewsModel.countDocuments();
         res.status(200).json({
             success: true,
