55e9d1e4e3863255509ba64d7462ecb624008c97
Starmoon9159 <112765789+Starmoon9159@users.noreply.github.com>
2024-11-24 03:04:40 +0800

    Update:Add cron

--- server.ts
+++ server.ts
@@ -2,11 +2,13 @@ import app from './src/app';
 import connectDatabase from './src/database/mongodb'
 import cors from 'cors'
 import runCheck from './check';
+/*定時任務 */
+import './src/cron/updateEYnews';
 app.use(cors())
 require('dotenv').config()
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
     console.log(`Server running at Port:${PORT}`);
     connectDatabase();
-    //runCheck()    
+    runCheck()
 });
---
+++ src/cron/updateEYnews.ts
@@ -0,0 +1,14 @@
+import cron from 'node-cron';
+import fetchEYNews from '../actions/EY/getEYnews';
+import updateEYnewsContent from '../actions/EY/getEYNewscontent';
+cron.schedule('0 17 * * *', () => {    
+    console.log('Starting to fetch EY news...');
+    fetchEYNews();
+    console.log('Finished fetching EY news.');
+    console.log('Starting to update EY news content...');
+    updateEYnewsContent();
+    console.log('Finished updating EY news content.');
+}, {
+    scheduled: true,
+    timezone: "Asia/Taipei"  // 設定為台灣時區
+});
