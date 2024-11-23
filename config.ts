acbcd21fead75615c7d27da1aad94f7594548108
Starmoon0911 <starmoon9159@gmail.com>
2024-11-23 19:10:56 +0800

    Update:Add vision model config

--- config.ts
+++ config.ts
@@ -3,21 +3,25 @@ const agent = {
     //if u aren't use this model u can don't change the value
     gemini: {
         model: "",
+        visionModel:"",
         apikey: process.env.gemini_apikey || "",
         base_url: ""
     },
     ollama: {
         model: "llama3",
+        visionModel:"",
         apikey: process.env.ollama_apikey || "ollama",
         base_url: "http://localhost:11434/v1"
     },
     grok: {
         model: "grok-beta",
+        visionModel:"",
         apikey: process.env.grok_apikey || "",
         base_url: "https://api.x.ai"
     },
     openai: {
         model: "",
+        visionModel:"",
         apikey: process.env.openai_apikey || "",
         base_url: ""
     }
